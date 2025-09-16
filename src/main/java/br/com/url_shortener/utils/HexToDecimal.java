2554353955f1c7575327f3c5532036a22c61c038
caezarof <renatocesardof@gmail.com>
2025-09-16 17:25:06 -0300

    refactor(HexToDecimal): replace magic number with named constant
    - Replace literal 16 with HEXADECIMAL constant for better readability
    - Maintain same functionality with improved code clarity

--- src/main/java/br/com/url_shortener/utils/HexToDecimal.java
+++ src/main/java/br/com/url_shortener/utils/HexToDecimal.java
@@ -3,7 +3,8 @@ package br.com.url_shortener.utils;
 import java.math.BigInteger;
 
 public class HexToDecimal {
+    private final static int HEXADECIMAL = 16;
     public static BigInteger convert(String hexString){
-        return new BigInteger(hexString, 16);
+        return new BigInteger(hexString, HEXADECIMAL);
     }
 }
