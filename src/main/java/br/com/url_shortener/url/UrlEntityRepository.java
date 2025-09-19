1c7d089b37a4b3a285fb2737d9aaf28f7b47d80d
caezarof <renatocesardof@gmail.com>
2025-09-19 18:06:53 -0300

    feat: add existsByShortCode method to UrlEntityRepository
    - Add existsByShortCode method to check for duplicate short codes
    - Enable duplicate prevention in URL shortening service
    - Support unique short code generation with retry mechanism

--- src/main/java/br/com/url_shortener/url/UrlEntityRepository.java
+++ src/main/java/br/com/url_shortener/url/UrlEntityRepository.java
@@ -5,4 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UrlEntityRepository extends JpaRepository<UrlEntity, Long> {
+
+    boolean existsByShortCode(String shortCode);
 }
