8bc5f3cb89da0640e92e3157e9cb5846855ec689
caezarof <renatocesardof@gmail.com>
2025-09-22 18:02:46 -0300

    feat: create DTO for shorten url created response
    - add CreatedShortenUrlDTO with id, originalUrl, shortCode and createdAtFormatted.
    - Format Instant to pattern: "HH:mm:ss dd/MM/yyyy" in America/Sao Paulo timezone
    - Constructor accepts UrlEntity and auto-formats the date

---
+++ src/main/java/br/com/url_shortener/url/CreatedShortenUrlDTO.java
@@ -0,0 +1,21 @@
+package br.com.url_shortener.url;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public record CreatedShortenUrlDTO(Long id, String originalUrl, String shortCode, String createdAtFormatted) {
+
+    public CreatedShortenUrlDTO(UrlEntity shortenUrl){
+        this(shortenUrl.getId(), shortenUrl.getOriginalUrl(), shortenUrl.getShortCode(),
+                formatInstantToSaoPaulo(shortenUrl.getCreatedAt()));
+    }
+
+    private static String formatInstantToSaoPaulo(Instant instant) {
+        ZoneId saoPauloZone = ZoneId.of("America/Sao_Paulo");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy")
+                .withZone(saoPauloZone);
+
+        return formatter.format(instant);
+    }
+}
